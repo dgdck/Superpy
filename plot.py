6b2d8714254f364adbfd5eb874ec711010dfb576
dgdck <d.godecke@gmail.com>
2023-02-16 17:34:07 +0100

    Revised revenue plot

--- plot.py
+++ plot.py
@@ -7,32 +7,46 @@ from main import csvreader
 
 
 def main():
-     my_plotter()
+     plot_revenue()
     
 
-def my_plotter():
+def plot_revenue():
     data = csvreader('revenue.csv')
     x = []
-    y = []
+    y_costs = []
+    y_income = []
+    y_lost = []
+    y_revenue = []
+    
     for item in data:
            x.append(datetime.datetime.strptime(item['date'], '%Y-%m-%d'))
-           y.append(float(item['revenue']))
-    
+           y_costs.append(float(item['costs']))
+           y_income.append(float(item['income']))
+           y_lost.append(float(item['lost']))
+           y_revenue.append(float(item['revenue']))
+
     # create plots:
     figure, axes = plt.subplots()
         
-    # format date
+    # format date:
     locator = AutoDateLocator()
     axes.xaxis.set_major_locator(locator)
     axes.xaxis.set_major_formatter(AutoDateFormatter(locator))
-    
     figure.autofmt_xdate()
+
+    # add data:
+    axes.plot(x, y_costs, marker = 'o', label='costs')
+    axes.plot(x, y_income, marker = 'o', label='sales')
+    axes.plot(x, y_lost, marker = 'o', label='lost')
+    axes.plot(x, y_revenue, marker = 'o', label='revenue')    
+
+    # plot make-up:
+    axes.set_xlabel('time')
+    axes.set_ylabel('EUR')
+    axes.set_title('Financial overview')
+    axes.legend()
     
-    plt.plot(x, y, marker='o')
-    plt.title('Revenue')
-    plt.xlabel("time")
-    plt.ylabel("EUR")
-    plt.grid()
+    # show plot:
     plt.show()
 
 
